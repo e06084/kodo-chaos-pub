5364a3b7dedde664f62d18aeb44834c651fc94b3
chupei <chupei@qiniu.com>
2020-03-06 14:01:18 +0800

    add init script

---
+++ README.md
@@ -0,0 +1,1 @@
+# kodo-chaos-pub
---
+++ script/init.sh
@@ -0,0 +1,21 @@
+# 该脚本使用工具s3-benchmark对kodo s3apiv2接口进行性能测试
+# 参考：https://github.com/minio/s3-benchmark
+
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+# set env
+export PWD=$(dirname "${BASH_SOURCE[0]}")
+export WORKSPACE=${PWD}/..
+source ${PWD}/util.sh
+
+info "download s3-benchmark tool"
+curl -Lo /usr/local/bin/s3-benchmark http://plivx6dnj.bkt.clouddn.com/s3-benchmark && chmod a+x /usr/local/bin/s3-benchmark
+
+info "start performance test with s3-benchmark"
+s3-benchmark -a CDF4IkThrImiuLquQWK-XTVqJvbBbYMFBSts370D -s NrgObTdYBZRg0Sf7gQMSGo6hs-jw6Oi55s506Q1b -b s3-benchmark -t 10 -u http://s3.in.tc.echosoul.cn -d 6000
+
+
+info "s3-benchmark test complete!"
