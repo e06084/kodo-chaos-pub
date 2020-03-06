bbf65bad637fbd6629a3170b474dfbcdc8e76124
chupei <chupei@qiniu.com>
2020-03-06 20:51:59 +0800

    fix

--- script/init.sh
+++ script/init.sh
@@ -10,13 +10,14 @@ set -o pipefail
 export PWD=$(dirname "${BASH_SOURCE[0]}")
 export WORKSPACE=${PWD}/..
 source ${PWD}/util.sh
+bucketName="s3-benchmark-"`date +%s%N`
 
 info "download s3-benchmark tool"
 curl -Lo /usr/local/bin/s3-benchmark http://plivx6dnj.bkt.clouddn.com/s3-benchmark && chmod a+x /usr/local/bin/s3-benchmark
 
 info "start performance test with s3-benchmark"
 info "Pls monitor service request @ http://prometheus.in.tc.echosoul.cn/graph"
-s3-benchmark -a CDF4IkThrImiuLquQWK-XTVqJvbBbYMFBSts370D -s NrgObTdYBZRg0Sf7gQMSGo6hs-jw6Oi55s506Q1b -b s3-benchmark -t 10 -u http://s3.in.tc.echosoul.cn -d 600
+s3-benchmark -a CDF4IkThrImiuLquQWK-XTVqJvbBbYMFBSts370D -s NrgObTdYBZRg0Sf7gQMSGo6hs-jw6Oi55s506Q1b -b ${bucketName} -t 10 -u http://s3.in.tc.echosoul.cn -d 600
 
 info "s3-benchmark test complete!"
 
